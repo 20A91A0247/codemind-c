606f50bcc602757c2e7fca7d7d770f3fdf3233c3
20A91A0247 <84468009+20A91A0247@users.noreply.github.com>
2023-01-08 20:18:12 +0530

    #include<iostream> using namespace std; int main() {     int n,n1,n2;     cin>>n;     while(n--)     {         //cin<<n;         cin>>n1>>n2;         cout<<n1+n2<<endl;     } }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,n1,n2;
+    cin>>n;
+    while(n--)
+    {
+        //cin<<n;
+        cin>>n1>>n2;
+        cout<<n1+n2<<endl;
+    }
+}
