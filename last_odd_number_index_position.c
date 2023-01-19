6d7958bcdf5fef42b60c2e1420a0c57242173ebb
20A91A0247 <84468009+20A91A0247@users.noreply.github.com>
2023-01-19 20:51:44 +0530

    #include<iostream> using namespace std; int main() {     int i,n,re=0;     cin>>n;     int a[n];     for(i=0;i<n;i++)     {         cin>>a[i];     }     int arr[n];     for(i=0;i<n;i++)     {         if(a[i]%2!=0)         {             re=i;         }     }     cout<<re; }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,re=0;
+    cin>>n;
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        if(a[i]%2!=0)
+        {
+            re=i;
+        }
+    }
+    cout<<re;
+}
